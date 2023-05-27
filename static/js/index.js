2807e3445aae5af9dc6a96af9b2f690441b97ef5
LazarusCat59 <zayedferosh@gmail.com>
2023-05-27 11:39:33 +0530

    Images change on button click

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,23 @@
-function changeImage(imgname)
+function changeImage(imgname, imgdesc)
 {
 	var img = document.getElementById("bigimg");
 	var path = "../static/images/".concat(imgname);
+	var desc = imgdesc;
 	img.src = path;
+	img.alt = desc;
 }
 
 function imgOne()
 {
-	changeImage("84bykpsrir1a1.jpg");
+	changeImage("84bykpsrir1a1.jpg", "Spider-Man: Into the Spider-Verse");
 }
 
 function imgTwo()
 {
-	changeImage("9fdeudnyny6a1.jpg");
+	changeImage("9fdeudnyny6a1.jpg", "Makima");
 }
 
 function imgThree()
 {
-	changeImage("ezOgN0q.jpg");
+	changeImage("ezOgN0q.jpg", "Sunset-ish");
 }
